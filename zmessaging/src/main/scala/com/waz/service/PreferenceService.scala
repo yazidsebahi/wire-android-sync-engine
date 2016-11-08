83e17a814d1bd5e214c3db14008fb0df7413eb74
Robin <robin@wire.com>
2016-11-08 10:00:18 +0100

    Change sendWithV3 to false as default in PreferenceService

--- zmessaging/src/main/scala/com/waz/service/PreferenceService.scala
+++ zmessaging/src/main/scala/com/waz/service/PreferenceService.scala
@@ -42,7 +42,7 @@ class PreferenceService(context: Context) {
 
   lazy val uiPreferences = uiPreferencesFrom(context)
 
-  def sendWithV3 = uiPreferences.getBoolean(sendWithAssetsV3Key, true) //true by default for production
+  def sendWithV3 = uiPreferences.getBoolean(sendWithAssetsV3Key, false) //true by default for production
 
   lazy val preferences = preferencesFrom(context)
 
