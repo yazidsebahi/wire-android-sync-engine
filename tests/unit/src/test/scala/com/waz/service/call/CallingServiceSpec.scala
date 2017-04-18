2033b2136d6685bba210d0759ae03691e6d82012
dean <deanrobertcook@gmail.com>
2017-04-18 10:47:53 +0200

    Add CallingServiceSpec skeleton

---
+++ tests/unit/src/test/scala/com/waz/service/call/CallingServiceSpec.scala
@@ -0,0 +1,32 @@
+/*
+ * Wire
+ * Copyright (C) 2016 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.waz.service.call
+
+import org.scalatest.{OptionValues, FeatureSpec, Matchers}
+
+class CallingServiceSpec extends FeatureSpec with Matchers {
+
+
+  feature("Group tests with features") {
+    scenario("Your test here") {
+      println("hello")
+      1 shouldEqual 1
+    }
+  }
+
+}
