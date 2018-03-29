dd08b2c5f7f391eb18479d31fe0f8bd9ce16dee4
dean <deanrobertcook@gmail.com>
2018-03-29 14:55:38 +0200

    Include /usr/lib again in Android.mk

--- zmessaging/src/main/jni/Android.mk
+++ zmessaging/src/main/jni/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE       := lzw-decoder
 LOCAL_SRC_FILES    := LzwDecoder.cpp
-LOCAL_LDLIBS       := -llog
+LOCAL_LDLIBS       := -L$(SYSROOT)/usr/lib -llog
 LOCAL_CFLAGS       := -O2 -Wall -pedantic -Wno-variadic-macros -fstack-protector-all
 include $(BUILD_SHARED_LIBRARY)
 
