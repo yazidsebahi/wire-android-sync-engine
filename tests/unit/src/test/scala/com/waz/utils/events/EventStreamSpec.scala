c54c94bda971aa80093d9d910fcb61eb27576fce
yeryomenkom <yeryomenkom@gmail.com>
2018-05-15 12:07:13 +0200

    Add more tests for EventStream.

--- tests/unit/src/test/scala/com/waz/utils/events/EventStreamSpec.scala
+++ tests/unit/src/test/scala/com/waz/utils/events/EventStreamSpec.scala
@@ -18,6 +18,7 @@
 package com.waz.utils.events
 
 import com.waz.specs.ZSpec
+import com.waz.utils.returning
 
 class EventStreamSpec extends ZSpec {
 
@@ -30,7 +31,7 @@ class EventStreamSpec extends ZSpec {
       val b: SourceStream[Int] = EventStream()
 
       val subscription = a.flatMap(_ => b) { _ => }
-      a ! 1 //emmit element so FlatMapLatestEventStream will
+      a ! 1
 
       withClue("mapped event stream should have subscriber after element emitting from source event stream.") {
         b.hasSubscribers shouldBe true
@@ -45,6 +46,49 @@ class EventStreamSpec extends ZSpec {
       withClue("mapped event stream should have no subscribers after onUnwire was called on FlatMapLatestEventStream") {
         b.hasSubscribers shouldBe false
       }
+    }
+
+    scenario("discard old mapped event stream when new element emitted from source event stream") {
+      val a: SourceStream[String] = EventStream()
+      val b: SourceStream[String] = EventStream()
+      val c: SourceStream[String] = EventStream()
+
+      var flatMapCalledCount = 0
+      var lastReceivedElement: Option[String] = None
+      val subscription = a.flatMap { _ =>
+        returning(if (flatMapCalledCount == 0) b else c) { _ => flatMapCalledCount += 1 }
+      } { elem => lastReceivedElement = Some(elem) }
+
+      a ! "a"
+
+      withClue("mapped event stream 'b' should have subscriber after first element emitting from source event stream.") {
+        b.hasSubscribers shouldBe true
+      }
+
+      b ! "b"
+
+      withClue("flatMapLatest event stream should provide events emitted from mapped signal 'b'") {
+        lastReceivedElement shouldBe Some("b")
+      }
+
+      a ! "a"
+
+      withClue("mapped event stream 'b' should have no subscribers after second element emitting from source event stream.") {
+        b.hasSubscribers shouldBe false
+      }
+
+      withClue("mapped event stream 'c' should have subscriber after second element emitting from source event stream.") {
+        c.hasSubscribers shouldBe true
+      }
+
+      c ! "c"
+      b ! "b"
+
+      withClue("flatMapLatest event stream should provide events emitted from mapped signal 'c'") {
+        lastReceivedElement shouldBe Some("c")
+      }
+
+      subscription.unsubscribe()
     }
 
   }
